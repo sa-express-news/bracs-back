2be4ec604bc7826818916f231ccac76e66ec6f84
Kia Farhang <kfarhang0@gmail.com>
2017-09-17 09:38:01 -0500

    Updated social links

--- src/components/SocialBlock/index.jsx
+++ src/components/SocialBlock/index.jsx
@@ -9,8 +9,8 @@ import './SocialBlock.css';
 export default () => (
   <div className="SocialBlock">
     <Social type='logo' link='http://www.expressnews.com/' />
-    <Social type='facebook' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
-    <Social type='twitter' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
-    <Social type='reddit' link='http://www.expressnews.com/st-anthony-basketball-ticket-to-top/' />
+    <Social type='facebook' link='http://www.expressnews.com/brac-looms-development-complicates-randolph-future/' />
+    <Social type='twitter' link='http://www.expressnews.com/brac-looms-development-complicates-randolph-future/' />
+    <Social type='reddit' link='http://www.expressnews.com/brac-looms-development-complicates-randolph-future/' />
   </div>
 ); 
